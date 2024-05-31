a195c94c6015cf2adde6d02c30d49323e4fc57f1
= <=>
2024-05-31 20:56:19 +0300

    changes

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -8,9 +8,6 @@
         enable = true;
 
         extensions = with pkgs.vscode-extensions; [
-            dracula-theme.theme-dracula
-            vscodevim.vim
-            yzhang.markdown-all-in-one
             vscode.nix
         ];
     };
